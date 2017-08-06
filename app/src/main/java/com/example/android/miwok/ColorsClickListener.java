bb904a864b4ba6b88cc4cad7240dd8bcb95f9902
cressterrell <cressterrell@gmail.com>
2017-08-05 21:27:00 -0500

    Initial commit

---
+++ app/src/main/java/com/example/android/miwok/ColorsClickListener.java
@@ -0,0 +1,14 @@
+package com.example.android.miwok;
+
+import android.view.View;
+import android.widget.Toast;
+
+/**
+ * Created by Cress on 8/3/2017.
+ */
+
+public class ColorsClickListener implements View.OnClickListener{
+    @Override
+    public void onClick(View view) {
+        Toast.makeText(view.getContext(), "Coming Soon", Toast.LENGTH_SHORT).show();
+}}
